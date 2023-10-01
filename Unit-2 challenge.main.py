82b6beb57e21bd5b92963fcba77f090f4bd6e620
Thirrumurugan <144253358+Thirrumurugan@users.noreply.github.com>
2023-10-01 22:15:30 +0530

    Update Unit-2 challenge.main.py

--- Unit-2 challenge.main.py
+++ Unit-2 challenge.main.py
@@ -4,21 +4,22 @@ class bankaccount:
     self.__account_number =account_number
     self.__account_holder_name =account_holder_name
     self.__account_balance =initial_balance
+  
 
-def deposit(self, amount):
-  if amount>0:
-    self. __account_balance +=amount
-    print("deposited ₹ {}.new balance:₹{}". format (amount,self.__account_balance))
-  else:
-    print ("invalid deposit amount.please deposit a positive amount")
-    def withdraw(self, amount ):
-      if amount >0 and amount <=self.__account_balance:
-        self.__account_balance -=amount
-        print ("withdraw ₹{}.new balance:₹{} ". format (amount,self.__account_balance ))
-      else:
-        print ("invalid withdrawal amount or insufficient balance")
-        def display_balance(self):
-          print("account balance for {}(account #{}):₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance ))
+  def deposit(self, amount):
+    if amount>0:
+      self. __account_balance +=amount
+      print("deposited ₹ {}.new balance:₹{}". format (amount,self.__account_balance))
+    else:
+      print ("invalid deposit amount.please deposit a positive amount")
+  def withdraw(self, amount ):
+    if amount >0 and amount <=self.__account_balance:
+      self.__account_balance -=amount
+      print ("withdraw ₹{}.new balance:₹{} ". format (amount,self.__account_balance ))
+    else:
+      print ("invalid withdrawal amount or insufficient balance")
+  def display_balance(self):
+    print("account balance for {}(account #{}):₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance ))
 
 #create an instance of the Bank account class
 account=Bankaccount(account_number ="123456789",account_holder_name="thirru", initial_balance=5000.0)
